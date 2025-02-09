b55179df55f8227200e83f4107644c2d3d82c4da
Daniel Idahosa <75020969+Daneil98@users.noreply.github.com>
2025-02-09 23:50:45 +0100

    Update urls.py

--- HMS1/api/urls.py
+++ HMS1/api/urls.py
@@ -1,17 +1,20 @@
 from django.urls import path
-
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 from . import views
 
-app_name = 'HMS1'
+app_name = 'api'
 
 urlpatterns = [
+    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),        #Good
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),       #Good
+    
     path('login/', views.LoginView.as_view(), name='login'),
     path('patient_register/', views.PatientRegisterView.as_view(), name='patient_register'),
     path('doctor_register/', views.DoctorRegisterView.as_view(), name='doctor_register'),
     
     path('dashboard/', views.Dashboard.as_view(), name='dashboard'),
     
-    path('patient_form/', views.PatientForm.as_view(), name='patient_form'),
+    path('patient_form/', views.Patient.as_view(), name='patient_form'),
     path('patient_profile/', views.PatientProfile.as_view(), name='patient_profile'),
     
     path('appointment/', views.Appointment.as_view(), name='appointment'),
